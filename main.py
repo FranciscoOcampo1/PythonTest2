cf5017ede343d1090adcb9eb7216d3c7fc39ce04
Francisco Ocampo <cheeto@Franciscos-MacBook-Pro.local>
2023-10-03 14:10:39 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+first_name = input("Francisco: ")
+last_name = input("Ocampo: ")
+
+print("Hello", first_name, last_name, "Nice to meet you")
+
+first_name = input("Francisco")
+last_name = input("Ocampo")
+age = input("20" + first_name + '?')
+print("hello", first_name, last_name, "nice to meet you!")
+print(first_name, 'you were born in the year ', 2023-int(age))
